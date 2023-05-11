0d372f65398988056b4ee5d76e523402d543b097
Georg Wiese <georgwiese@gmail.com>
2023-05-11 14:07:57 +0200

    Add Params

--- src/gadgets/bit_selector.rs
+++ src/gadgets/bit_selector.rs
@@ -138,6 +138,7 @@ mod tests {
     impl<F: PrimeField> Circuit<F> for MyCircuit<F> {
         type Config = Config;
         type FloorPlanner = SimpleFloorPlanner;
+        type Params = ();
 
         fn without_witnesses(&self) -> Self {
             Self::default()
