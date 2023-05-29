37ae3a47156c3590ef93fa9d48b802d5db4600fe
Georg Wiese <georgwiese@gmail.com>
2023-05-29 10:33:22 +0200

    Add integration test

---
+++ tests/integration_test.rs
@@ -0,0 +1,23 @@
+use std::path::PathBuf;
+
+use zero_g::io::{image::load_image, model::load_wnn};
+
+const IMG_PATH: &str = "benches/example_image_7.png";
+const MODEL_PATH_MNIST_TINY: &str = "models/model_28input_256entry_1hash_1bpi.pickle.hdf5";
+const MODEL_PATH_MNIST_SMALL: &str = "models/model_28input_1024entry_2hash_2bpi.pickle.hdf5";
+
+/// Analogously to the benchmark, mock-proofs the checked-in image using the "MNIST Tiny" model.
+#[test]
+fn mock_proof_mnist_tiny() {
+    let img = load_image(&PathBuf::from(IMG_PATH)).unwrap();
+    let wnn = load_wnn(&PathBuf::from(MODEL_PATH_MNIST_TINY)).unwrap();
+    wnn.mock_proof(&img, 12);
+}
+
+/// Mock-proofs the checked-in image using the "MNIST Small" model.
+#[test]
+fn mock_proof_mnist_small() {
+    let img = load_image(&PathBuf::from(IMG_PATH)).unwrap();
+    let wnn = load_wnn(&PathBuf::from(MODEL_PATH_MNIST_SMALL)).unwrap();
+    wnn.mock_proof(&img, 17);
+}
