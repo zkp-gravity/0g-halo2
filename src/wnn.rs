bcbbdaee7cbbeecf31cb4d8726cc9e5ffb8df2b5
Georg Wiese <georgwiese@gmail.com>
2023-05-29 09:00:58 +0000

    Fix example

--- src/wnn.rs
+++ src/wnn.rs
@@ -48,19 +48,21 @@ pub struct Wnn {
 /// use halo2_proofs::poly::{
 ///     commitment::ParamsProver, kzg::commitment::ParamsKZG,
 /// };
+/// use std::path::Path;
 ///
 /// let img = load_image(&Path::new("benches/example_image_7.png")).unwrap();
 /// let wnn = load_wnn(&Path::new("models/model_28input_256entry_1hash_1bpi.pickle.hdf5")).unwrap();
+/// let k = 12;
 ///
 /// // Asserts that all constraints are satisfied
-/// wnn.mock_proof(&img, 12);
+/// wnn.mock_proof(&img, k);
 ///
 /// // Generate keys
 /// let kzg_params = ParamsKZG::new(k);
 /// let pk = wnn.generate_proving_key(&kzg_params);
 ///
 /// // Generate proof
-/// let (proof, outputs) = wnn.proof(&pk, &kzg_params, image);
+/// let (proof, outputs) = wnn.proof(&pk, &kzg_params, &img);
 ///
 /// // Verify proof
 /// wnn.verify_proof(&proof, &kzg_params, pk.get_vk(), &outputs);
