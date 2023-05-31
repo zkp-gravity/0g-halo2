ebd76cb9855d8c4ec907369327218980d9ed4c9b
Georg Wiese <georgwiese@gmail.com>
2023-05-31 13:29:12 +0200

    Add MNIST-Medium and MNIST-Large models (#23)
    * Add MNIST medium and large models
    
    * Update models with bugfix in BTHOWeN repo
    
    * Update readme, benchmarks & tests

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,36 +6,37 @@ use halo2_proofs::{
     poly::{commitment::ParamsProver, kzg::commitment::ParamsKZG},
 };
 use ndarray::Array2;
-use zero_g::{load_grayscale_image, load_wnn, Wnn};
+use zero_g::{checked_in_test_data::*, load_grayscale_image, load_wnn, Wnn};
 
-fn setup() -> (Wnn, Array2<u8>, ParamsKZG<Bn256>) {
-    let model_path = PathBuf::from("models/model_28input_256entry_1hash_1bpi.hdf5");
-    let img_path = PathBuf::from("benches/example_image_7.png");
+fn setup(model_info: (u32, &str)) -> (Wnn, Array2<u8>, ParamsKZG<Bn256>) {
+    let (k, model_path) = model_info;
+    let model_path = PathBuf::from(model_path);
+    let img_path = PathBuf::from(TEST_IMG_PATH);
 
     let wnn = load_wnn(&model_path).unwrap();
     let img = load_grayscale_image(&img_path).unwrap();
 
-    let kzg_params = ParamsKZG::new(12);
+    let kzg_params = ParamsKZG::new(k);
 
     (wnn, img, kzg_params)
 }
 
-fn bench_key_generation(b: &mut Bencher) {
-    let (wnn, _img, kzg_params) = setup();
+fn bench_key_generation(b: &mut Bencher, model_info: (u32, &str)) {
+    let (wnn, _img, kzg_params) = setup(model_info);
 
     b.iter(|| wnn.generate_proving_key(&kzg_params));
 }
 
-fn bench_proof_generation(b: &mut Bencher) {
-    let (wnn, img, kzg_params) = setup();
+fn bench_proof_generation(b: &mut Bencher, model_info: (u32, &str)) {
+    let (wnn, img, kzg_params) = setup(model_info);
 
     let pk = wnn.generate_proving_key(&kzg_params);
 
     b.iter(|| wnn.proof(&pk, &kzg_params, &img));
 }
 
-fn bench_verification(b: &mut Bencher) {
-    let (wnn, img, kzg_params) = setup();
+fn bench_verification(b: &mut Bencher, model_info: (u32, &str)) {
+    let (wnn, img, kzg_params) = setup(model_info);
 
     let pk = wnn.generate_proving_key(&kzg_params);
     let (proof, outputs) = wnn.proof(&pk, &kzg_params, &img);
@@ -47,9 +48,29 @@ fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("benches");
     group.sample_size(10);
 
-    group.bench_function("proof_generation", bench_proof_generation);
-    group.bench_function("key_generation", bench_key_generation);
-    group.bench_function("verification", bench_verification);
+    group.bench_function("key_generation_mnist_tiny", |b| {
+        bench_key_generation(b, MNIST_TINY)
+    });
+    group.bench_function("proof_generation_mnist_tiny", |b| {
+        bench_proof_generation(b, MNIST_TINY)
+    });
+    group.bench_function("verification_mnist_tiny", |b| {
+        bench_verification(b, MNIST_TINY)
+    });
+
+    group.bench_function("proof_generation_mnist_small", |b| {
+        bench_proof_generation(b, MNIST_SMALL)
+    });
+    group.bench_function("verification_mnist_small", |b| {
+        bench_verification(b, MNIST_SMALL)
+    });
+
+    group.bench_function("proof_generation_mnist_medium", |b| {
+        bench_proof_generation(b, MNIST_MEDIUM)
+    });
+    group.bench_function("verification_mnist_medium", |b| {
+        bench_verification(b, MNIST_MEDIUM)
+    });
 
     group.finish();
 }
