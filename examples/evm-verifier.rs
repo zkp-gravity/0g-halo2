626c860afc4b885eb2977fa968618558754fbeb0
Georg Wiese <georgwiese@gmail.com>
2023-06-07 16:33:42 +0000

    Simplify EVM example

--- examples/evm-verifier.rs
+++ examples/evm-verifier.rs
@@ -4,8 +4,7 @@ use ethers::{
 };
 use eyre::Result;
 use halo2_proofs::{
-    halo2curves::bn256::{Bn256, Fr, G1Affine},
-    plonk::VerifyingKey,
+    halo2curves::bn256::Bn256,
     poly::{commitment::ParamsProver, kzg::commitment::ParamsKZG},
 };
 use std::{env, path::Path, str::FromStr};
@@ -15,13 +14,23 @@ use zero_g::{
     load_grayscale_image, load_wnn,
 };
 
-async fn validate_evm(
-    kzg_params: ParamsKZG<Bn256>,
-    vk: &VerifyingKey<G1Affine>,
-    proof: Vec<u8>,
-    instances: Vec<Vec<Fr>>,
-    endpoint: Option<&String>,
-) {
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    println!("Loading model and image...");
+    let (k, model_path) = MNIST_MEDIUM;
+    let wnn = load_wnn(Path::new(model_path)).unwrap();
+    let image = load_grayscale_image(Path::new(TEST_IMG_PATH)).unwrap();
+
+    println!("Generating keys & proof...");
+    let kzg_params = ParamsKZG::<Bn256>::new(k);
+    let pk = wnn.generate_proving_key(&kzg_params);
+    let (proof, instance_column) = wnn.proof(&pk, &kzg_params, &image);
+
+    let instances = vec![instance_column];
+    let vk = pk.get_vk();
+
     println!("Generating deployment code...");
     let deployment_code = gen_evm_verifier(&kzg_params, vk, vec![instances[0].len()]);
 
@@ -30,7 +39,7 @@ async fn validate_evm(
         dry_run_verifier(deployment_code.clone(), instances.clone(), proof.clone()).unwrap();
     println!("Gas used: {}", gas_used);
 
-    if let Some(endpoint) = endpoint {
+    if let Some(endpoint) = args.get(1) {
         let anvil = Anvil::new().spawn();
 
         let (endpoint, wallet) = if endpoint == "anvil" {
@@ -55,27 +64,5 @@ async fn validate_evm(
             .await
             .unwrap();
     }
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let (k, model_path) = MNIST_MEDIUM;
-
-    let wnn = load_wnn(Path::new(model_path)).unwrap();
-    let image = load_grayscale_image(Path::new(TEST_IMG_PATH)).unwrap();
-
-    let kzg_params = ParamsKZG::<Bn256>::new(k);
-    let pk = wnn.generate_proving_key(&kzg_params);
-    let (proof, instance_column) = wnn.proof(&pk, &kzg_params, &image);
-
-    validate_evm(
-        kzg_params,
-        pk.get_vk(),
-        proof,
-        vec![instance_column],
-        args.get(1),
-    )
-    .await;
     Ok(())
 }
